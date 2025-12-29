ecb43d89239466d63fc93fae27413c22629f5ae4
JHANKAR01 <jhadejhankar@gmail.com>
2025-12-30 02:09:03 +0530

    7 login fixed now implemting google sign up

--- models/User.ts
+++ models/User.ts
@@ -1,13 +1,13 @@
 import mongoose, { Schema, models, model } from 'mongoose';
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, unique: true, sparse: true },
+  password: { type: String },
   name: { type: String, required: true },
   role: { type: String, enum: ['admin', 'partner', 'patient'], default: 'patient' },
   operationalRole: { type: String, default: 'none' },
   phone: String,
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   address: String
 }, { timestamps: true });
 
