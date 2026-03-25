88d8f1eb2b93f54aa7a1684552de75c4bc363b00
Jhankar Jhade <jhadejhankar@gmail.com>
2026-03-25 17:42:49 +0530

    Update route.ts

--- app/api/tests/route.ts
+++ app/api/tests/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect';
 import Test from '@/models/Test';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   await dbConnect();
   try {
